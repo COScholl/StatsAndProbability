2baa236fcf640c9b116201d676fc19b1ce8202b2
Chris Scholl <chris.o.scholl@gmail.com>
2020-06-17 14:51:14 -0400

    Re-organize add inline documentation
    Inline documentation will end up in README.md
    For now, I am documenting inline for context and to help figure out other functions of use

--- probability.js
+++ probability.js
@@ -1,6 +1,6 @@
 const BigNumber = require('bignumber.js');
 
-/* Working with probability and probability space */
+/* Helper functions */
 
 /**
  * Multiply base b by itself e number of times
@@ -26,21 +26,124 @@ const forFact = (num) => {
 };
 
 /**
+ * Generate array of numbers in range start and stop by step
+ * @param  {number} start    start of number range
+ * @param  {number} stop     end of number range
+ * @param  {number} [step=1] diff between sequential elems in retArr
+ * @return {Array}           array of numbers
+ */
+const range = (start, stop, step = 1) => {
+    let retArr = [start],
+        rangeElem = start;
+    while (rangeElem < stop) {
+      // updates rangeElem by step and pushes into retArr
+      retArr.push(rangeElem += step);
+    }
+
+    return retArr;
+};
+
+/*
+  Probability space is composed of a sample space Ω, the event space ℱ, and
+  the probability function P.
+  Ω is the set of all possible outcomes.
+  ℱ is a set of specific outcome parameters or events of the sample space to consider.
+  ℱ contains sample space, that is, Ω ∈ ℱ.
+  P is a function that determines the probability of each event in ℱ.
+  P:Ω → [0,1], that is P assigns values to possible events of Ω between 0 and 1 inclusive.
+  P:ℱ → [0,1], that is P assigns values to specific event parameters of ℱ between 0 and 1 inclusive.
+  P(Ω) = 1, that is the sum of probability for all possible subsets of Ω is 1.
+  P(∅) = 0, that is an empty set has a probability of 0.
+  For each subset A (A ⊂ Ω), A is called an event.
+  P(A) = probability of A, where 0 < P(A) ≤ 1.
+  For any element ω (ω ∈ Ω), ω is called an atomic event.
+  P(ω) = P({ω}), and where A = {ω₁,ω₂,...,ωₙ}, {ω₁,ω₂,...,ωₙ} ⊂ Ω.
+  P(A) = P(ω₁) + P(ω₂) + ... + P(ωₙ).
+  Complement event- all events that are not event in question.
+  P(Aᶜ) = 1 - P(A), so P(Aᶜ) = P(Ω) - P(A).
+  Disjoint events- events that do not have a common outcome.
+  P(A or B) = P(A ⋃ B) = P(A) + P(B).
+  P(A and B) = P(A ⋂ B) = 0.
+  Intersection events- common outcomes in each event.
+  P(A and B) = P(A ⋂ B) = P(A) * P(B).
+  Union events- outomes are in either (logical or) event.
+  P(A or B) = P(A ⋃ B) = P(A) + P(B) - P(A ⋂ B).
+*/
+
+/**
+ * Let X be a random variable with finite number of finite outcomes x₁,x₂,...,xₖ
+ * each with its respective probability p₁,p₂,...,pₖ
+ * Expectation of X (E[X]) or mean of random variable x (μₓ) is:
+ *         k
+ * E[X] =  ∑ xᵢpᵢ = x₁p₁ + x₂p₂ + ... + xₖpₖ
+ *        i=1
+ * Determine expected value of random outcome for a given probability space
+ * {'x₁': p₁, 'x₂': p₂, ..., 'xₖ': pₖ}
+ * @param  {Object} pSpace probability space for all potential outcomes
+ * @return {number}        weighted average of all potential outcomes
+ */
+const expectedDiscreteValue = (pSpace) => {
+  let retVal = BigNumber(0);
+  for (let x in pSpace) {
+    retVal = retVal.plus(BigNumber(x).multipliedBy(pSpace[x]));
+  }
+
+  return retVal.toNumber();
+};
+
+/*
+  Binomial distribution models the number of k successes in n trials where each
+  trial is independent (no possible outcome is removed from the set of possible
+  outcomes in each trial). Binomial distribution is dependent on the binomial
+  coefficient.
+
+  For a discrete random variable X, it is a binomially distributed (X ~ B(n, p))
+  if it satisfies the following 4 conditions:
+  1. There is a fixed number of trials (n)
+  2. Each trial has two possible outcomes: success (k) or failure
+  3. The probability of success (p) is the same for each trial
+  4. Trials are independent, meaning the outcome of one trial doesn't influence
+     the outcome of another trial
+ */
+
+/* Binomial coefficients */
+
+/**
  * ⎛n⎞        n!
  * ⎜ ⎟ =  ――――――――――
  * ⎝k⎠    k!(n - k)!
  * "n choose k":
- * "Out of 3 (n) tosses, what are the chances of 2 (k) heads?"
- * This does not allow for repetitions
- * @param  {number} n total number of trials/options
- * @param  {number} k target number for success/ chosen outcome
- * @return {number}   chances of k success out of n trials
+ * binomial coefficent is the number of ways to choose unique k elements from a fixed
+ * set of n elements
+ * @param  {number} n total number of options
+ * @param  {number} k subset of unique k elements from set of n options
+ * @return {number}   possible number of ways to choose k elements from set of n elements
  */
 const binomialCoefficient = (n, k) => {
   let numerator = forFact(n);
-  let denominator = forFact(n-k) *  forFact(k);
+  let denominator = forFact(n-k) * forFact(k);
 
   return BigNumber(numerator).dividedBy(denominator).toNumber();
+};
+
+/**
+ *  k  n + 1 - i
+ * 	Π ――――――――――
+ * i=1    i
+ * "n choose k":
+ * binomial coefficent is the number of ways to choose unique k elements from a fixed
+ * set of n elements
+ * @param  {number} n total number of options
+ * @param  {number} k subset of unique k elements from set of n options
+ * @return {number}   possible number of ways to choose k elements from set of n elements
+ */
+const binomialCoefficientMultiplicative = (n, k) => {
+  let retVal = 1;
+  for (let i = 1; i <= k; k++) {
+    retVal = BigNumber(retVal).multiply((BigNumber(n).plus(1).minus(i)).dividedBy(i));
+  }
+
+  return retVal.toNumber();
 };
 
 /**
@@ -48,17 +151,60 @@ const binomialCoefficient = (n, k) => {
  * ⎜⎜ ⎟⎟ = ⎜           ⎟
  * ⎝⎝k⎠⎠   ⎝     k     ⎠
  * "n multichoose k":
+ * multinomial coefficient is the number of ways to choose k elements with repeats
+ * from a fixed set of n elements
  * "Out of 9 (n) flavors of icecream, how many combinations are there with
  * 3 (k) scoops, allowing for multiple scoops of each flavor?"
- * This allows for repetition
- * @param  {number} n total number of trials/options
- * @param  {number} k target number for success/chosen outcome
- * @return {number}   chances of k choices out of n options
+ * @param  {number} n options
+ * @param  {number} k subset of potentially repeating elements from set of n options
+ * @return {number}   possible number of ways to choose repeatable k elements from set of n elements
  */
 const multisetCoefficient = (n, k) => {
 
   return binomialCoefficient(n + k - 1, k);
 };
+
+/**
+ *            ⎛n⎞
+ * P(X = k) = ⎜ ⎟pᵏ(1-p)ⁿ⁻ᵏ
+ *            ⎝k⎠
+ * Models probability of success or failure in experimental survey repeated
+ * multiple times for getting exactly k successes in n trials
+ * @param  {number} n fixed number of trials
+ * @param  {number} k successes
+ * @param  {number} p probability for k
+ * @return {number}   probability of successes for k in n trials
+ */
+const binomialDistMassFunc = (n, k, p) => {
+  // number of outcomes for k successes in n trials
+  let coeff = binomialCoefficient(n, k);
+  // q is probability of failure, complement of p
+  let q = 1.0 - p;
+  let succ = exponentiate(p, k);
+  let fail = exponentiate(q, (n - k));
+
+  return coeff * succ * fail;
+};
+
+/**
+ * ⎣k⎦ ⎛n⎞
+ *  ∑  ⎜ ⎟ pⁱ(1-p)ⁿ⁻ⁱ
+ * i=0 ⎝i⎠
+ * The probability that that random-value X will take a value less than or equal to k
+ * @param  {number} n fixed number of trials
+ * @param  {number} k successes
+ * @param  {number} p probability for k
+ */
+const binomialDistCDF = (n, k, p) => {
+  let retVal = 0;
+  for (let i = 0; i <= k; i++) {
+    retVal += binomialDistMassFunc(n, i, p);
+  }
+
+  return retVal;
+}
+
+/* Dice-related functions */
 
 /**
  *        kₘₐₓ     ⎛n⎞ ⎛p - s * k - 1⎞
@@ -82,30 +228,12 @@ const pOfSumInDice = (p, n, s) => {
 };
 
 /**
- * Generate array of numbers in range start and stop by step
- * @param  {number} start    start of number range
- * @param  {number} stop     end of number range
- * @param  {number} [step=1] diff between sequential elems in retArr
- * @return {Array}           array of numbers
- */
-const range = (start, stop, step = 1) => {
-    let retArr = [start],
-        rangeElem = start;
-    while (rangeElem < stop) {
-      // updates rangeElem by step and pushes into retArr
-      retArr.push(rangeElem += step);
-    }
-
-    return retArr;
-};
-
-/**
  * Create probability space for sums of numbers of n dice with s number of sides
  * @param  {number} n number of dice
  * @param  {number} s number of sides of dice
  * @return {Object}   probability space for sums of n number of dice with s sides
  */
-const pForAllSumsInDice = (n, s) => {
+const probabilitySpace = (n, s) => {
   let sums = range(n, n * s);
   let pSums = sums.map((sum) => pOfSumInDice(sum, n, s));
   let retObj = {};
@@ -114,41 +242,11 @@ const pForAllSumsInDice = (n, s) => {
   return retObj;
 };
 
-/**
- *                                    ⎛n⎞
- * f(k,n,p) = Pr(k;n,p) = Pr(X = k) = ⎜ ⎟pᵏ(1-p)ⁿ⁻ᵏ
- *                                    ⎝k⎠
- * Models probability of success or failure in experimental survey repeated
- * multiple times
- * @param  {number} n number of trials
- * @param  {number} k successes
- * @param  {number} p probability for k
- * @return {number}   probability of successes for k in n trials
- */
-const binomialDistribution = (n, k, p) => {
-  // number of outcomes for k successes in n trials
-  let coeff = binomialCoefficient(n, k);
-  // q is probability of failure, complement of p
-  let q = 1.0 - p;
-  let succ = exponentiate(p, k);
-  let fail = exponentiate(q, (n - k));
-
-  return coeff * succ * fail;
-};
-
-/**
- * Determine expected value of random outcome for a given probability space
- * @param  {Object} pSpace probability space for all potential outcomes
- * @return {number}        weighted average of all potential outcomes
- */
-const expectedDiscreteValue = (pSpace) => {
-  let retVal = BigNumber(0);
-  for (let x in pSpace) {
-    retVal = retVal.plus(BigNumber(x).multipliedBy(pSpace[x]));
-  }
-
-  return retVal.toNumber();
-};
+// @XXX: TODO
+// make function(s) that will generate a probability space from arrays of objects that describe discrete sample spaces
+// [coin: {events: [H,T], number: 5}],
+// [dice: {events; [1,2,3,4,5,6], number: 3}],
+// [redMarbles: {events: [red], number: 3}, blueMarbles: {events: [blue], number: 4}]
 
 /* Parsing data sets that are arrays of numbers */
 
@@ -316,8 +414,6 @@ const twoPointFive = (data = []) => {
   return dataSort.slice(low, high);
 };
 
-// console.log(twoPointFive([3,3,4,4,5,6,7,104]));
-
 /**
  * Remove elements that are greater than 3 standard deviations in value
  * @param  {Array}  [data=[]] integer or float data points
@@ -347,5 +443,3 @@ const interQuartRange = (data = []) => {
 
   return dataSort.filter((elem) => elem > md - (2 * interQuart) && elem < mn + (2 * interQuart));
 };
-
-console.log(interQuartRange([3,3,4,4,5,6,7,104]));
